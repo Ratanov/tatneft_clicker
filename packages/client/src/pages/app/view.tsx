0f76feec5a22c4388e0e96166aadc37280ecd1b6
Oleg <displacer88@gmail.com>
2024-10-25 16:09:51 +0300

    feat: added file structure (#4)
    * fix
    
    * fix
    
    * feat: added file structure
    
    ---------
    
    Co-authored-by: Ratanov Oleg V <ratanovov@tatneft.ru>

---
+++ packages/client/src/pages/app/view.tsx
@@ -0,0 +1,5 @@
+import './style.css'
+
+export const App: React.FC = () => {
+  return <div className="game">Вот тут будет жить кликер</div>
+}
---
+++ packages/client/src/pages/profile/view.tsx
@@ -0,0 +1,5 @@
+import './style.css'
+
+export const Profile: React.FC = () => {
+  return <div className="profile">Профиль</div>
+}
